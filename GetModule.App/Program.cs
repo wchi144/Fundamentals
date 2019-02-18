65b8d5dd42f5f64850ff7b2ea3e3f5fe535cd005
TAKUYA\Linda <linda.c218@gmail.com>
2019-02-18 18:45:27 +1300

    new project

---
+++ GetModule.App/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace GetModules
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //printSomeTuple("linda", 26);
+            var root = Assembly.GetExecutingAssembly().Location;
+            root = Path.GetDirectoryName(root);
+
+            if (!Directory.Exists(root))
+            {
+                throw new FileNotFoundException($"folder {root} does not exisit");
+            }
+
+            var list = GetAllAssemblies(root);
+            printAllAssemblies(list);
+
+            
+        }
+
+        static void printSomeTuple(string name, int age)
+        {
+            var subjectNameAndAge = (Name: name, Age: age);
+            Console.WriteLine($"{subjectNameAndAge.Name} is {subjectNameAndAge.Age} years old.");
+        }
+
+        static IList<Assembly> GetAllAssemblies(string path)
+        {
+            var assemblies = new List<Assembly>();
+
+            foreach (var dll in Directory.GetFiles(path, "*.dll"))
+            {
+                assemblies.Add(Assembly.LoadFile(dll));
+            }
+
+            return assemblies;
+        }
+
+        static void printAllAssemblies(IList<Assembly> assemblies)
+        {
+
+            foreach (var dll in assemblies)
+            {
+                Debug.WriteLine(dll);
+            }
+        }
+    }
+}
